42c49e8c24670e304b09601b56e558004ec94311
aster-max <icatmuhammad2@gmail.com>
2021-04-08 23:38:18 +0700

    rdy to aclculateas

--- source/run-diagnose.sh
+++ source/run-diagnose.sh
@@ -15,8 +15,9 @@ read -p "║::  tinggi_badan ............ : " get_height
 read -p "║::  berat_badan ............. : " get_weight
 read -p "║::  suhu_badan_kepala ....... : " get_temp
 read -p "║::  frekuensi_ambil_nafas ... : " get_freqbreath
-read -p "║::  frekuensi_denyut_nadi ... : " get_freqbreath
-read -p "║::  saturasi_oksigen ........ : " get_freqbreath
+read -p "║::  frekuensi_denyut_nadi ... : " get_freqheartbeat
+read -p "║::  saturasi_oksigen ........ : " get_saturation
+read -p "║::  lama_mengalami_gejala ... : " get_sicktime
 echo -e "║::  $foot2"
 printf  "║::  ${YELLOW}-${ENDCOLOR} %-25s ${YELLOW}-${ENDCOLOR} %-25s ${YELLOW}-${ENDCOLOR} %-26s\n" "batuk_sulit_bernafas" "masalah_berat_badan" "masalah_sulit_tidur" "masalah_nafsu_makan" "menangis_berlebihan" "masalah_mmuntah_diare"
 echo -e "║::  $foot2"
@@ -52,7 +53,7 @@ if [[ "$check_gejala" != "null" ]]; then
 	done
 	echo -e "$comp" | head -n -2 > "${file_path[7]}"
 
-	while [[ "$itr" -le "$each_ctr_penyakit" ]]; do
+	while [[ "$itr" -lt "$idx" ]]; do
 		param_suhu=$(jq ".$main_gejala[$itr].param_suhu" "${file_path[4]}")
 		param_saturasi=$(jq ".$main_gejala[$itr].param_saturasi" "${file_path[4]}")
 		if [[ "$get_age" -lt 1 ]]; then
@@ -60,9 +61,38 @@ if [[ "$check_gejala" != "null" ]]; then
 		else
 			param_nafas=$(jq ".$main_gejala[$itr].param_nafas[1]" "${file_path[4]}")
 		fi
-		## main calculation goes here beb
+		## kalkulasi param
+
+		## kalulasi regex
+		catch_symtomp=""
+		while IFS= read -r each_symtomp; do
+			##jadiin multiple pattern
+			deli_each=$(echo "$each_symtomp" | cut -d '"' -f 2)
+			catch_symtomp+="|$deli_each"
+		done < "${file_path[7]}"
+		fin_symtomp=$(echo "$catch_symtomp" | cut -c 2-)
+		echo "$fin_symtomp"
+
+		## lemmetization matching + cf setter
+		itr_regex=0	
+		arr_match=("")
+		idx_regex=$(jq ".$main_gejala[$itr] | length" "${file_path[5]}")
+		while [[ "$itr_regex" -lt  "$idx_regex" ]]; do
+			#statements
+			#grep each symmtpp ganti jadi fin symptop
+			search_gejala=$(jq ".$main_gejala[$itr].gejala_$itr_regex" "${file_path[5]}" | grep -E "$fin_symtomp")
+			if [[ "$search_gejala" ]]; then
+				# mulai itung cf
+				echo "$itr_regex"
+				cf_rule=$(cat "${file_path[5]}" | grep -E -w "$fin_symtomp" | wc -l)
+			fi
+			itr_regex=$((++itr_regex))
+		done
+
 		itr=$((++itr))
 	done
 else
 	echo ""
 fi
+
+# desimal : echo "(4/6)*100" | bc -l | cut -c 1-5
